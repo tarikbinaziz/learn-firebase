04663e1fa2227a7d82b8c5d7b17e7f6603a8e706
tarik <tarik@gmail.com>
2023-07-23 12:21:04 +0600

    search items

--- lib/database/show_post.dart
+++ lib/database/show_post.dart
@@ -14,6 +14,7 @@ class ShowPost extends StatefulWidget {
 class _ShowPostState extends State<ShowPost> {
   final auth = FirebaseDatabase.instance;
   final ref = FirebaseDatabase.instance.ref("post");
+  final searchCon=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,42 +28,40 @@ class _ShowPostState extends State<ShowPost> {
       appBar: AppBar(
         title: Text("Your post"),
       ),
-      body: Column(
-        children: [
-          Expanded(
-              child: StreamBuilder(
-                  stream: ref.onValue,
-                  builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: searchCon,
+              decoration: InputDecoration(
+                  hintText: "search", border: OutlineInputBorder()),
+              onChanged: (String value){
+                setState(() {
 
-                    Map<dynamic,dynamic> map=snapshot.data!.snapshot.value as dynamic;
-                    List<dynamic> list=[];
-                    list.clear();
-                    list=map.values.toList();
-
-                    if(!snapshot.hasData){
-                      return CircularProgressIndicator();
-                    }else{
-                      return ListView.builder(
-                          itemCount: snapshot.data!.snapshot.children.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text(list[index]['title']),
-                              subtitle: Text(list[index]['id'].toString()),
-                            );
-                          });
+                });
+              },
+            ),
+            Expanded(
+              child: FirebaseAnimatedList(
+                  query: ref,
+                  itemBuilder: (_, snapshot, animation, index) {
+                    final title=snapshot.child("title").value.toString();
+                    if(searchCon.text.isEmpty){
+                      return ListTile(
+                        title: Text(snapshot.child("title").value.toString()),
+                      );
+                    }else if(title.toLowerCase().contains(searchCon.text.toLowerCase())){
+                      return ListTile(
+                        title: Text(snapshot.child("title").value.toString()),
+                      );
                     }
+                    return Container();
 
-                  })),
-          Expanded(
-            child: FirebaseAnimatedList(
-                query: ref,
-                itemBuilder: (_, snapshot, animation, index) {
-                  return ListTile(
-                    title: Text(snapshot.child("title").value.toString()),
-                  );
-                }),
-          )
-        ],
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
