f21b6122fb7155979bf1bf9a033ac2bdcf43fafe
App_1 <a@gmail.com>
2023-09-12 18:03:43 +0600

    added push notification

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,10 +13,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-
   @override
   void initState() {
+    /// forground
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        var snacbar = SnackBar(
+          content: Text(message.notification!.title.toString()),
+          duration: Duration(days: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snacbar);
+        print(message.notification!.title);
+      }
+    });
+
+    /// background
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      if (message.notification != null) {
+        var snacbar = SnackBar(
+          content: Text(message.notification!.title.toString()),
+          duration: Duration(days: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snacbar);
+        print(message.notification!.title);
+      }
+    });
+
+    /// terminated
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        var snacbar = SnackBar(
+          content: Text(message.notification!.title.toString()),
+          duration: Duration(days: 1),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snacbar);
+        print(message.notification!.title);
+      }
+    });
     super.initState();
   }
 
@@ -27,9 +63,9 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () async {
               final SharedPreferences prefs =
                   await SharedPreferences.getInstance();
-           FirebaseAuth.instance.signOut();
-              Navigator.push(context, MaterialPageRoute(builder: (_)=>SigninEmailPass()));
-
+              FirebaseAuth.instance.signOut();
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => SigninEmailPass()));
             },
             icon: Icon(Icons.logout)),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'database/chat screen.dart';
@@ -15,16 +16,23 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.brown),
-        home:ImageStore()
+        home:HomeScreen()
         //RealTimePractise()
         // ShowPost()
 
